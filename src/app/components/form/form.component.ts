ef6119a433150aecbe4ce8cca6c8a4d54d7f6de0
Eduardo Sandoval <edubizzle7@gmail.com>
2020-06-15 12:03:21 -0500

    dia finalizado

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { CountryService } from "../../services/country.service";
+import { FormGroup, FormControl } from "@angular/forms";
 @Component({
   selector: "app-form",
   templateUrl: "./form.component.html",
@@ -7,6 +8,8 @@ import { CountryService } from "../../services/country.service";
 })
 export class FormComponent implements OnInit {
   public CountriesName: Array<any> = [];
+  //
+  public formulario: FormGroup;
 
   constructor(private CountryService: CountryService) {
     this.CountryService.GetCountry().subscribe((country) =>
@@ -14,5 +17,17 @@ export class FormComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  public createForm() {
+    // esto recibe un objeto de formcontrols
+    /*****
+    que son los encargados de controlar los inputs o elementos que existen en el formulario
+    ******/
+    this.formulario = new FormGroup({
+      First_name: new FormControl("")
+    });
+  }
 }
